2400e53b0a59a64f8d322fe7a4f9b104fcebf130
CapitaoJamal <lolopercussi@hotmail.com>
2017-08-12 12:40:32 -0300

    Teletransporte e a morte da cobra.

--- cobrinha.js
+++ cobrinha.js
@@ -1,57 +1,77 @@
-
 tamanho = 600;
-escala = 20;
+escala = 14;
 
 function setup() {
     createCanvas(tamanho,tamanho);
-    frameRate(18);
+    frameRate(10);
     
     cobrinha = {
-        cor:color(0,255,0),
+        cor: color(0,255,0),
         corpo: [],
         dir: createVector(0,0),
+        viva: true,
         cria: function() {
-            for(i=0;i<4;i++) {
-                x=tamanho/2+i*escala;
-                y=tamanho/2;
+            for(i=0;i<40;i++) {
+                x = tamanho / 2 + i*escala;
+                y = tamanho / 2;
                 q = createVector(x,y);
                 this.corpo.push(q);
-       
             }
             this.setDir(dirs['a']);
         },
         desenha: function() {
             fill(this.cor);
-            this.anda();
-            for(i=0;i<this.corpo.length;i++){
+            if(this.viva) this.anda();
+            for(i=0;i<this.corpo.length;i++) {
                 q = this.corpo[i];
                 rect(q.x,q.y,escala,escala);
             }
         },
-        anda : function(){
+        anda: function() {
             q = this.corpo[0].copy();
             q.add(this.dir);
             this.corpo.unshift(q);
             
             this.corpo.splice(-1,1);
+            
+            if(this.corpo[0].x < 0){
+                this.corpo[0].x = tamanho - escala;
+            }
+            if(this.corpo[0].x >= tamanho){
+                this.corpo[0].x = 0;
+            }
+            if(this.corpo[0].y < 0){
+                this.corpo[0].y = tamanho - escala;
+            }
+            if(this.corpo[0].y>= tamanho){
+                this.corpo[0].y = 0;
+            }
+            
+            for(i=1;i<this.corpo.length;i++){
+                if(this.corpo[0].dist(this.corpo[i]) == 0){
+                    this.viva = false;
+                    this.cor = color(189,183,107);
+                }
+            }
         },
         setDir: function(d) {
-            angulo=degrees(p5.Vector.angleBetween(this.dir,d));
+            angulo = degrees(p5.Vector.angleBetween(this.dir,d));
             if (angulo != 180) {
                 this.dir = d.copy();
                 this.dir.mult(escala);
             }
         }
     };
-    dirs= {
-    'w' : createVector(0,-1),
-    'a' : createVector(-1,0),
-    's' : createVector(0,1) ,      
-    'd' : createVector(1,0)
-        
-    };
     
+    dirs = {
+        'w' : createVector(0,-1),
+        'a' : createVector(-1,0),
+        's' : createVector(0,1),
+        'd' : createVector(1,0)
+    };
+
     cobrinha.cria();
+    
 }
 
 function draw() {
@@ -61,12 +81,12 @@ function draw() {
 
 function keyPressed() {
     switch(key.toLowerCase()) {
-        case 'w':    
+        case 'w':
         case 'a':
         case 's':
         case 'd':
-            d=dirs[key.toLowerCase()];
-            cobrinha.setDir(d);
+            d = dirs[key.toLowerCase()]
+            cobrinha.setDir(d)
             break;
     }
 }
